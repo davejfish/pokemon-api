05bf6155e9c48279bec0d4bfef815ba223d11917
Dave Fisher <dave.j.fish@gmail.com>
2022-07-27 16:56:22 -0700

    mvp finished

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,33 +3,29 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-const { cats } = require('../data/cats');
-
 describe('cats routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('/cats should return a list of cats', async () => {
-    const res = await request(app).get('/cats');
-    const expected = cats.map((cat) => {
-      return { id: cat.id, name: cat.name };
+  it('/pokemon should return a list of pokemon', async () => {
+    const res = await request(app).get('/pokemon');
+    
+    expect(res.body.length).toEqual(10);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
     });
-    expect(res.body).toEqual(expected);
   });
 
-  it('/cats/:id should return cat detail', async () => {
-    const res = await request(app).get('/cats/1');
-    const felix = {
+  it('/pokemon/:id should return pokemon detail', async () => {
+    const res = await request(app).get('/pokemon/1');
+
+    expect(res.body).toEqual({
       id: '1',
-      name: 'Felix',
-      type: 'Tuxedo',
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Felix_the_cat.svg/200px-Felix_the_cat.svg.png',
-      year: 1892,
-      lives: 3,
-      isSidekick: false,
-    };
-    expect(res.body).toEqual(felix);
+      name: 'bulbasaur',
+      type: 'grass',
+    });
   });
 
   afterAll(() => {
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,22 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP table if exists pokemon;
+
+CREATE table pokemon (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    type VARCHAR NOT NULL
+);
+
+INSERT INTO pokemon (name, type) VALUES
+('bulbasaur', 'grass'),
+('ivysaur', 'grass'),
+('venasaur', 'grass'),
+('charmander', 'fire'),
+('charmeleon', 'fire'),
+('charizard', 'fire'),
+('squirtle', 'water'),
+('wartortle', 'water'),
+('blastoise', 'water'),
+('caterpie', 'bug');
